48a4cdc2bead162d295ad9ee1210ac5ea86caa53
dre-zouhair <dre.zouhair@gmail.com>
2021-11-14 16:16:53 +0100

    Adding the Entity annotation and defining the primary key with the Id annotation

--- src/main/java/com/tut/springdata/entity/Student.java
+++ src/main/java/com/tut/springdata/entity/Student.java
@@ -3,6 +3,9 @@ package com.tut.springdata.entity;
 import lombok.*;
 import lombok.experimental.Accessors;
 
+// use javax.persistence to make it easier to change the JPA implementation in the future
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Getter
@@ -12,8 +15,10 @@ import java.util.UUID;
 @RequiredArgsConstructor
 // or use @Data instead of this 5 annotations
 @Accessors(chain = true)
+@Entity // to map to a table
 public class Student {
 
+    @Id
     private UUID id;
     private String firstName;
     private String lastName;
